935afe604ff33f7438b0d0bf1c1f74eb218b98c6
Nathalia Campos <nathalia@4intelligence.com.br>
2022-01-20 10:25:10 -0300

    feat: add tests

---
+++ tests/unit/test_normalize_strings.py
@@ -0,0 +1,31 @@
+
+import pytest
+
+from pylib import normalize_strings
+
+
+@pytest.fixture
+def string_snake():
+    return 'Atenção \N{SNAKE}'
+
+# @pytest.fixture
+# def string_aeiou():
+#     return 'áéíóú'
+
+
+def test_remove_non_ascii(string_snake):
+
+    # EXERCISE
+    new_string = normalize_strings.remove_non_ascii(string_snake)
+
+    # ASSERT
+    assert new_string == 'ateno '
+
+
+def test_non_ascii_normalized(string_snake):
+
+    # EXERCISE
+    new_string = normalize_strings.remove_non_ascii_normalized(string_snake)
+
+    # ASSERT
+    assert new_string == 'atencao '
